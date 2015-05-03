8d5cfb513884a820379512f4037ff6a8384ab74b
bmcorser <bmcorser@gmail.com>
2015-05-03 23:14:09 +0100

    Lazy release

---
+++ test/func/test_lazy_release.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import os
+from click.testing import CliRunner
+import tags
+
+
+def test_cli_rerelease(monkeypatch, function_repo):
+    'Rereleasing a package returns proper exit code and message'
+    function_repo.packages('a', 'b', 'c')
+    monkeypatch.setattr(tags.message, 'capture_message', lambda: 'User message')
+    runner = CliRunner()
+    runner.invoke(tags.cli.main, ['release', 'a'])
+    result = runner.invoke(tags.cli.main, ['release', 'a'])
+    assert result.exit_code == os.EX_USAGE
+    assert 'package hasn’t changed' in result.output
