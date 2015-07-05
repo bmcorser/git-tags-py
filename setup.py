028f9f55c82232821783b51c82fe1bcb709777ca
bmcorser <bmcorser@gmail.com>
2015-07-05 19:29:04 +0100

    Whatevs

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ URL = 'https://github.com/bmcorser/git-tags-py'
 setup(
     name='git-tags',
     url=URL,
-    description="{0}, see {1} for more information.".format(DESC, URL),
+    long_description="{0}, see {1} for more information.".format(DESC, URL),
     author='bmcorser',
     author_email='bmcorser@gmail.com',
-    short_description=DESC,
+    description=DESC,
     version=VERSION,
     packages=['tags', 'tags.cli'],
     install_requires=['click', 'pyyaml'],
