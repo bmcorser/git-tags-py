7350b45248b213e8239aafd2c9059bcb630e1def
bmcorser <bmcorser@gmail.com>
2015-07-05 21:38:28 +0100

    Bump for PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.3'
+VERSION = '0.1.0'
 DESC = 'CLI for managing release tags in a Git repository'
 URL = 'https://github.com/bmcorser/git-tags-py'
 setup(
