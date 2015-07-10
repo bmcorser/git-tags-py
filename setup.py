e3b5a3dc2e3da6cce70dae19e63edfa019367bc8
bmcorser <bmcorser@gmail.com>
2015-07-10 09:57:04 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 DESC = 'CLI for managing release tags in a Git repository'
 URL = 'https://github.com/bmcorser/git-tags-py'
 setup(
