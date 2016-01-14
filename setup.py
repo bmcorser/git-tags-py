061a98b5c111fc91ff90f47a9200c512bd7320fa
bmcorser <bmcorser@gmail.com>
2016-01-14 16:31:28 +0000

    Hack around notes ending up in inconsistent state

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 DESC = 'CLI for managing release tags in a Git repository'
 URL = 'https://github.com/bmcorser/git-tags-py'
 setup(
